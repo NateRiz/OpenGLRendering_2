0d331fbbb44ce1c3057a4aeeca75775c5c467b5f
NateRiz <riziknat@msu.edu>
2018-07-29 17:59:38 -0400

    rename to fieldmanager

---
+++ Geometry/Headers/FieldManager.h
@@ -0,0 +1,14 @@
+//
+// Created by nathan on 7/29/18.
+//
+
+#ifndef VOXEL3_CLION_FIELDMANAGER_H
+#define VOXEL3_CLION_FIELDMANAGER_H
+
+
+class FieldManager {
+
+};
+
+
+#endif //VOXEL3_CLION_FIELDMANAGER_H
---
+++ Geometry/Sources/FieldManager.cpp
@@ -0,0 +1,5 @@
+//
+// Created by nathan on 7/29/18.
+//
+
+#include "../Headers/FieldManager.h"
